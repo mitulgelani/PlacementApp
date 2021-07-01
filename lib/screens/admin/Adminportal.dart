985474960b0f63e2b2c3df5d9fe648001e60d683
mitulgelani <mitulgelani53@gmail.com>
2021-07-01 05:31:19 -0700

    My Branch

--- lib/screens/admin/Adminportal.dart
+++ lib/screens/admin/Adminportal.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_search_bar/flutter_search_bar.dart';
 import 'package:placementapp/screens/admin/Adminpost.dart';
 
 class AdminPortal extends StatefulWidget {
@@ -16,102 +15,6 @@ class AdminPortal extends StatefulWidget {
 class _AdminPortalState extends State<AdminPortal> {
   final FirebaseUser user;
   _AdminPortalState(this.user);
-
-  SearchBar searchBar;
-
-  final db = Firestore.instance;
-  List<DocumentSnapshot> documents;
-  List<DocumentSnapshot> friends;
-
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  String name, uid;
-
-  TextEditingController _searchQueryController = TextEditingController();
-  bool _isSearching = false, _show = false;
-  String searchQuery = "Search query";
-
-  Widget _buildSearchField() {
-    return TextField(
-      controller: _searchQueryController,
-      autofocus: true,
-      decoration: InputDecoration(
-        hintText: "Search Friend Name",
-        border: InputBorder.none,
-        hintStyle: TextStyle(color: Colors.white30),
-      ),
-      style: TextStyle(color: Colors.white, fontSize: 26.0),
-      onChanged: (query) => updateSearchQuery(query),
-    );
-  }
-
-  List<Widget> _buildActions() {
-    if (_isSearching) {
-      return <Widget>[
-        IconButton(
-          icon: const Icon(Icons.clear),
-          onPressed: () {
-            if (_searchQueryController == null ||
-                _searchQueryController.text.isEmpty) {
-              Navigator.pop(context);
-              friends.clear();
-              return;
-            }
-            _clearSearchQuery();
-          },
-        ),
-      ];
-    }
-
-    return <Widget>[
-      IconButton(
-        icon: const Icon(Icons.search),
-        onPressed: _startSearch,
-      ),
-    ];
-  }
-
-  void _startSearch() {
-    /*  ModalRoute.of(context)
-        .addLocalHistoryEntry(LocalHistoryEntry(onRemove: _stopSearching)); */
-
-    setState(() {
-      _isSearching = true;
-    });
-  }
-
-  void updateSearchQuery(String newQuery) {
-    setState(() {
-      _show = true;
-      searchQuery = newQuery;
-      //  friendlist();
-    });
-  }
-
-  void _stopSearching() {
-    _clearSearchQuery();
-
-    setState(() {
-      _isSearching = false;
-      //    friendlist();
-    });
-  }
-
-  void _clearSearchQuery() {
-    setState(() {
-      _searchQueryController.clear();
-      updateSearchQuery("");
-    });
-  }
-
-  /*  Future friendlist() async {
-    QuerySnapshot result;
-    if(_searchQueryController.text=="")
-      result=await db.collection('users').getDocuments();
-    else
-      result=await db.collection('users').where('name',isEqualTo:_searchQueryController.text).getDocuments();
-    return result.documents;
-  } 
-   */
 
   Future getdata() async {
     final QuerySnapshot result =
@@ -175,16 +78,15 @@ class _AdminPortalState extends State<AdminPortal> {
             return ListView.builder(
               itemCount: snapshot.data.length,
               itemBuilder: (context, i) {
-                /*       String cname = snapshot.data[i]['cname'];
+                String cname = snapshot.data[i]['cname'];
                 String cdetail = snapshot.data[i]['cdetails'];
+                String url = snapshot.data[i]['cimage'];
                 // ignore: deprecated_member_use
                 List<dynamic> semlist = List<dynamic>();
                 // ignore: deprecated_member_use
                 List<dynamic> branchlist = List<dynamic>();
                 semlist = snapshot.data[i]['sem'];
-                branchlist = snapshot.data[i]['branch']; */
-                String url = snapshot.data[i]['cimage'];
-
+                branchlist = snapshot.data[i]['branch'];
                 return Card(
                   elevation: 5,
                   child: Stack(
@@ -223,14 +125,14 @@ class _AdminPortalState extends State<AdminPortal> {
                         children: [
                           SizedBox(height: 100),
                           Container(
-                            child: Text('Company:\n',
+                            child: Text('Company:\n${cname}',
                                 style: TextStyle(
                                     fontSize: 20,
                                     fontWeight: FontWeight.w900,
                                     color: Colors.white)),
                           ),
                           Container(
-                            child: Text('Company Details:\n',
+                            child: Text('Company Details:\n${cdetail}',
                                 overflow: TextOverflow.ellipsis,
                                 style: TextStyle(
                                     fontWeight: FontWeight.w900,
